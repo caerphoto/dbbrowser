75364b32f5bc230915966f98d21d8cf2543a03a6
Andy <andrew.farrell@phgroup.com>
2016-09-09 13:47:49 +0100

    Change table model to pass more params.

--- src/javascript/models/table.js
+++ src/javascript/models/table.js
@@ -6,20 +6,28 @@ var utils = require('utils');
 module.exports = Backbone.Model.extend({
     defaults: {
         name: '',
-        user: '',
-        type: '',
+        type: 'table',
         sql: null
+    },
+    initialize: function (options) {
+        this.bookmark = options.bookmark;
     },
     loadViewSQL: function () {
         var path;
         var model = this;
 
         if (this.get('type') !== 'view') {
-            return;
+            throw new TypeError('Cannot fetch SQL of object type "' +
+                this.get('type') + '".');
         }
 
-        path = ['', this.get('user'), 'viewText', this.get('name')].join('/');
-        $.get(path).done(function (sql) {
+        path = '/viewText';
+        $.get(path, {
+            name: this.get('name'),
+            user: this.bookmark.get('user'),
+            password: this.bookmark.get('password'),
+            connection: this.bookmark.get('connection')
+        }).done(function (sql) {
             model.set('sql', sql);
         }).fail(function (xhr, errorText) {
             utils.alertDialog('Error:\n\n' + errorText, 'error');
